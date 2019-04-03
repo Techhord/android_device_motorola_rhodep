5c886790181655c899524721b7b5676317860bf5
Bowgo Tsai <bowgotsai@google.com>
2019-04-03 16:44:10 +0800

    rhodep: Adding GSI keys
    Bug:112293933
    Test: build and checks the keys are installed into ramdisk
    Change-Id: I216f3900e9511e0929418af050ff964898d0aea1

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, vendor/motorola/rhodep/rhodep-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
