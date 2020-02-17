c92ebcb768e7e7730de0853855649dc1dfc75905
Wang Han <416810799@qq.com>
2020-02-17 12:19:46 +0800

    rhodep: Add recovery pixel format variant
     * according to CAF.
    
    Change-Id: I8fb8b0173d1c5eb9f38955e36166d526bebd50f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # Screen density
 TARGET_SCREEN_DENSITY := 440
