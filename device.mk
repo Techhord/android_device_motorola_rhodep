6d9257e1feccf709b049149ffc4368d1ad30aa9d
luca020400 <luca.stefani.ge1@gmail.com>
2022-10-11 16:24:48 -0300

    rhodep: Switch to NXP NFC stack Change-Id: I56908d6aa0a938ea89b35a41e3f90768bd8ce0ac

--- device.mk
+++ device.mk
@@ -80,6 +80,16 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     netutils-wrapper-1.0
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0:64 \
+    android.hardware.nfc@1.1:64 \
+    android.hardware.nfc@1.2:64 \
+    android.hardware.secure_element@1.0:64 \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
