472b6801e9dee9f4cac68188e0bcf941a9500961
Cow6oy <mezackisilva@gmail.com>
2022-10-12 20:04:10 -0300

    rhodep: overlay-lineage: Set vendor platform signature * from stock

--- device.mk
+++ device.mk
@@ -94,7 +94,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
